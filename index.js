52e46641609a918df6ed6aee495f58c25dae79dd
murrayholmes96 <murray.holmes@nature.scot>
2021-06-16 15:10:16 +0100

    Add postcode page to required and module export
    Issue: Scottish-Natural-Heritage/Licensing#699

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const formatters = require('./formatters.js');
 const recipients = require('./recipients.js');
+const postalAddress = require('./postal-address.js');
 
-module.exports = {formatters, recipients};
+module.exports = {formatters, recipients, postalAddress};
