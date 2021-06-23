2423aee8651281c82cec043d0a21531d5892e917
murrayholmes96 <murray.holmes@nature.scot>
2021-06-23 16:47:56 +0100

    Add descriptors for exporting functions from lib
    Issue: Scottish-Natural-Heritage/Licensing#699

--- index.d.ts
+++ index.d.ts
@@ -14,6 +14,29 @@ declare const utils: {
      */
     stripAndRemoveObscureWhitespace(value: string): string;
   };
+  postalAddress: {
+    /**
+     * Validates a postcode against the regex set in GOV.UK Notify.
+     *
+     * Originally found in `notifications_utils/postal_address.py#L154-L160`, at
+     * the above repo.
+     *
+     * @param {string} postcode The candidate postcode to be validated.
+     * @returns {boolean} `true` if the postcode is valid, otherwise `false`.
+     */
+    isaRealUkPostcode(value: string): boolean;
+
+    /**
+     * Formats the postcode to standard postcode format without whitespace removed.
+     *
+     * Originally found in `notifications_utils/postal_address.py#L163-L173`, at
+     * the above repo.
+     *
+     * @param {string} postcode The candidate postcode to be validated.
+     * @returns {string} A version of the postcode formatted.
+     */
+    formatPostcodeForPrinting(value: string): string;
+  };
   recipients: {
     /**
      * Validates and formats an email address in to the pattern required by
